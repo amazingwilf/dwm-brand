82f91670643922de34163b6b151961306fc4468f
David Elliott <david@starjump.org>
2024-10-22 15:49:54 +0100

    alt tags

--- config.def.h
+++ config.def.h
@@ -61,7 +61,13 @@ static const unsigned int alphas[][4]      = {
 
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8" };
+static const char *tags[] = { "·", "·", "·", "·", "·" };
+static const char *alttags[] = { "A", "B", "C", "D", "E" };
+
+static const unsigned int ulinepad	= 5;	/* horizontal padding between the underline and tag */
+static const unsigned int ulinestroke	= 2;	/* thickness / height of the underline */
+static const unsigned int ulinevoffset	= 0;	/* how far above the bottom of the bar the line should appear */
+static const int ulineall 		= 0;	/* 1 to show underline on all tags, 0 for just the active ones */
 
 static const Rule rules[] = {
 	/* xprop(1):
